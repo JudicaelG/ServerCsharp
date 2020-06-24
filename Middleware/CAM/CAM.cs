db45dfc1af109a240e8ff95922d3c968f80d1002
judicael <judicael.guillaume@viacesi.fr>
2020-06-24 17:15:23 +0200

    Ajout de log pour le cam (je ne sais pas si ça marche réellement)

--- Middleware/CAM/CAM.cs
+++ Middleware/CAM/CAM.cs
@@ -1,6 +1,7 @@
 ﻿using Middleware.CTRLWF;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Middleware.CAM
 {
     class CAM
     {
+        private static TraceSource source = new TraceSource("CAM");
         private static CAM instance = null;
         private static readonly object padlock = new object();
         private STCMSG message;
@@ -37,6 +39,7 @@ namespace Middleware.CAM
 
         public STCMSG dispatching(STCMSG message)
         {
+            source.TraceEvent(TraceEventType.Start, 0, "dispatching");
             this.message = message;
             if(message.App_name == "Appli1")
             {
@@ -44,11 +47,14 @@ namespace Middleware.CAM
                 {
                     if(message.Op_name == "authentifier")
                     {
+                        source.TraceEvent(TraceEventType.Start, 0, "authentifier");
                         this.exec = new Auth_V1();
                         this.message = this.exec.exec(this.message);
+                        source.TraceEvent(TraceEventType.Stop, 0, "authentifier");
                     }
                 }
             }
+            source.TraceEvent(TraceEventType.Stop, 0, "dispatching");
             return this.message;
         }
     }
